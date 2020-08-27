a8ed3d312195c80871f532d954882caf8c1f2a84
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-08-27 11:58:29 +0100

    add first maze

---
+++ src/example_maze/level.lean
@@ -0,0 +1,39 @@
+-- import the definition of the example maze
+import example_maze.definition
+
+/-!
+
+# Example maze.
+
+You are in a maze of twisty passages, all alike! 
+
+You can go north, south east or west.
+
+-/
+
+namespace maze
+
+open can_escape
+
+/- Solver remark : there are 5 rooms. -/
+
+/-- Use n,s,e,w to move around. When you're at the exit, type `out`. -/
+
+example : goal :=
+begin
+  -- ready...
+  unfold goal,
+  -- go!
+  s,
+  n,
+  s,
+  e,
+  n,
+  -- etc etc,
+  sorry,
+  -- Don't bang into the walls -- those are errors.
+  -- When you get there, the tactic to get you out is `out`. 
+  -- There is also a magic word, an ancient translation of the word `sorry`.
+end
+
+end maze
